6228dcceab596fb44ce87b7ad89f19cb09a9ab5a
Anderson Torres <106359275+And3rsonTorres@users.noreply.github.com>
2024-01-24 13:07:20 -0500

    Optimization
    -Change the Nav as it wont have any effect.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Nav from "../Components/nav";
 import ComingSoonPage from "../Components/CommingSoon";
+import HomeIcon from "../Components/HomeIcon";
 export default function Home() {
   return (
   <>
-  <Nav/>
+  <nav className="bg-redbase p-2 flex justify-center">
+      <HomeIcon />
+    </nav>
   <ComingSoonPage/>
   </>);
 }
