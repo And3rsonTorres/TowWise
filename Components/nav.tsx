b9b0e56cfe84893121ed779e5ae78dcbc7349840
Anderson Torres <106359275+And3rsonTorres@users.noreply.github.com>
2024-01-24 11:56:41 -0500

    Coming Soon
    -added Navigation bar
    -added coming soon text and animation
    -added Temp App Icon

---
+++ Components/nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomeIcon from '@/Assets/HomeIcon'
+const Nav: React.FC = () => {
+  return (
+    <nav className="bg-redbase p-2 flex justify-center">
+      <HomeIcon />
+    </nav>
+  );
+};
+
+export default Nav;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'redbase':"#44001A",
+        'yellow': "#F0F600"
+      }
     },
   },
   plugins: [],
